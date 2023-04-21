bf9bac87d5f83bf227eb086394e641b8138450e5
chaseburton <72556266+chaseburton@users.noreply.github.com>
2023-04-21 14:49:30 +0300

    Updating version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='omnisearch',
-    version='0.0.2',
+    version='0.0.3',
     description='A collection of state space search algorithms.',
     long_description=long_description,
     long_description_content_type="text/markdown",
